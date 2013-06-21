84bec149540545c2b2f6ce0988e0ff88e8724d12
James Barnsley <james@plasticstudio.co.nz>
2013-06-21 16:39:52 +1200

    Making it work

--- deploy.php
+++ deploy.php
@@ -11,8 +11,22 @@
  
 <pre>
 	<?php 
+		
+		// output
 		print_r( $output );
+		
+		// save to log
 		file_put_contents('/var/www/test/logs/git.log', $output, FILE_APPEND);
+		
+		// send an email
+		$to      = 'james@barnsley.co.nz';
+		$subject = 'Auto-Deployment';
+		$message = 'Succesfully pushed';
+		$headers = 'From: test@dev.jamesbarnsley.co.nz' . "\r\n" .
+			'Reply-To: test@dev.jamesbarnsley.co.nz' . "\r\n" .
+			'X-Mailer: PHP/' . phpversion();
+
+		mail($to, $subject, $message, $headers);		
 	?>
 </pre>
 
