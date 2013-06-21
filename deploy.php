756482fa75ce943e17e4b20cbd7943357276a622
James Barnsley <james@plasticstudio.co.nz>
2013-06-21 16:00:22 +1200

    Fixing script

--- deploy.php
+++ deploy.php
@@ -2,10 +2,10 @@
 
 <p>Pulling ... 
 
-<?php
-	
-	echo exec('git pull');
-
-?>
+<?php $output = shell_exec('/var/www/test/public_html/ && git pull 2>&1'); ?>
 
 done!</p>
+
+<pre>
+<?php echo $output; ?>
+</pre>
